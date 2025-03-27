8cbcc70c7ca1d11c58571550de807af961531430
Treys <treysxs@yandex.ru>
2025-03-28 00:04:36 +0300

    Init

---
+++ TiaMaxWebApplication/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu');
+const mobileContent = document.querySelector('.mobile__content');
+const mobile_content_main = document.querySelector('.mobile_content_main');
+
+menuBtn.addEventListener('click', () => {
+   menu.classList.toggle('menu--open');
+   mobileContent.classList.toggle('blure--content');
+});
+
+mobile_content_main.addEventListener('click', () => {
+   console.log(1);
+   if (menu.classList.contains('menu--open')) {
+      menu.classList.toggle('menu--open');
+      mobileContent.classList.toggle('blure--content');
+   }
+});
